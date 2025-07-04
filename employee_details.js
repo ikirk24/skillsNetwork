38a9ac5ed6a64f97bc678d98ca6e989c5bbf3a0e
Ian Kirk <iankirk24@gmail.com>
2025-07-04 02:09:28 -0400

    Practice Problem 1

--- employee_details.js
+++ employee_details.js
@@ -1,16 +1,15 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
-      //... More employee records can be added here
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specilization: 'Javascript' },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specilization: 'Python' },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specilization: 'Java' },
     ];
 
- function displayEmployees() {
+function displayEmployees() {
 const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
         document.getElementById('employeesDetails').innerHTML = totalEmployees;
   }
 
-  function calculateTotalSalaries() {
+function calculateTotalSalaries() {
       const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
       alert(`Total Salaries: $${totalSalaries}`);
     }
@@ -29,4 +28,14 @@ const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${
     else{
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
      }
+ }
+
+ function findBySpecialization (employeeSpecialization) {
+    const foundEmployee = employees.find(employee => employee.specilization === employeeSpecialization);
+    if (foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.department} - ${foundEmployee.specilization}</p>`;
+    } 
+    else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this Specialization';
+       }
  }
