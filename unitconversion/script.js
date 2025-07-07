509b41d8bb7e381d68cc80bf3f8e21ba7215471b
Ian Kirk <iankirk24@gmail.com>
2025-07-07 12:43:14 -0400

    added problems

---
+++ unitconversion/script.js
@@ -0,0 +1,7 @@
+function cToF () {
+    //Fetching elements 
+    var celsius = document.getElementById('c').value;
+    var fahrenheit = document.getElementById('f').value
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+}
